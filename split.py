272dea1305948522b9933ba3ad7692b5eb555167
indexphp <andi.uph@gmail.com>
2021-11-23 06:28:19 +0700

    Add files via upload

---
+++ split.py
@@ -0,0 +1,16 @@
+import json
+
+in_file_path='_metadata.json' # Change me!
+
+with open(in_file_path,'r') as in_json_file:
+
+    # Read the file and convert it to a dictionary
+    json_obj_list = json.load(in_json_file)
+
+    for json_obj in json_obj_list:
+        filename=json_obj['name'] # +'.json'
+
+        with open(filename, 'w') as out_json_file:
+            # Save each obj to their respective filepath
+            # with pretty formatting thanks to `indent=4`
+            json.dump(json_obj, out_json_file, indent=4)
